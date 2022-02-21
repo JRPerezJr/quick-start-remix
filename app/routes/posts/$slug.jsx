2c34b4a326cc061222a96fcf7fc643a04e36c00f
Juan Perez <jperezdev@vivaldi.net>
2022-02-21 14:17:48 +0900

    created dynamic post route

---
+++ app/routes/posts/$slug.jsx
@@ -0,0 +1,13 @@
+import { useLoaderData } from 'remix';
+import invariant from 'tiny-invariant';
+import { getPost } from '~/posts';
+
+export const loader = async ({ params }) => {
+  invariant(params.slug, 'expected params.slug');
+  return getPost(params.slug);
+};
+
+export default function PostSlug() {
+  const post = useLoaderData();
+  return <div dangerouslySetInnerHTML={{ __html: post.html }} />;
+}
